a763618e605c811369f8f572e0640e31a0660d5b
Nicolas <nicolasmurgueitio@hotmail.com>
2023-02-06 06:02:11 -0500

    Update BrainMicrobiome.R

--- BrainMicrobiome.R
+++ BrainMicrobiome.R
@@ -1,24 +1,22 @@
 #set up working directory where the genus and brain data is:
 setwd("")
 
-#FORMAT DATA LIKE IN TUTORIAL
-
 library("randomForest")
-library("plyr") # for the "arrange" function
-library("rfUtilities") # to test model significance
-library("caret") # to get leave-one-out cross-validation accuracies and also contains the nearZeroVar function 
+library("plyr") 
+library("rfUtilities")
+library("caret") 
 library("mosaic")
 
-#reading tutorial data
-#OTU
+#reading  data
+
 otu_table_brain <- read.table("otu_table_genus_brain.txt", sep="\t", header=T, row.names=1, stringsAsFactors=FALSE, comment.char="")  
 metadata_brain <- read.table("metadata_brain.txt", sep="\t", header=T, row.names=1, stringsAsFactors=TRUE, comment.char="")
 
 #explore data
-dim(otu_table_brain) #88 samples, 1858 OTUS
+dim(otu_table_brain) 
 
 #explore metadata
-dim(metadata_brain) #3 metadata columns
+dim(metadata_brain)
 
 #Insula model
 otu_table_scaled_insula <- data.frame(t(otu_table_brain))  
@@ -45,7 +43,7 @@ insula.r.squared.dist
 
 #Permutation test (1000) (TAKES A WHILE TO RUN)
 RF_insula_sig <- rf.significance( x=RF_insula ,  xdata=otu_table_scaled_insula[,1:(ncol(otu_table_scaled_insula)-1)] , nperm=1000 , ntree=501 )  
-RF_insula_sig #Model is signigicant at p=0.004 (r2=0.09)
+RF_insula_sig 
 saveRDS( file = "RF_insula_sig.rda" ,RF_insula_sig )
 
 #Accuracy Estimated by Cross-validation
@@ -63,7 +61,7 @@ importance(RF_insula)
 RF_insula_imp <- as.data.frame( RF_insula$importance )
 RF_insula_imp$features <- rownames( RF_insula_imp )
 RF_insula_imp_sorted <- arrange( RF_insula_imp  , desc(`%IncMSE`)  )
-RF_insula_imp_sorted #top %IncMSE =60758.146060
+RF_insula_imp_sorted 
 
 barplot(RF_insula_imp_sorted$`%IncMSE`, ylab="% Increase in Mean Squared Error (Variable Importance)", main="RF Regression Variable Importance Distribution")
 top_10_features_insula<-barplot(RF_insula_imp_sorted[1:10,"%IncMSE"], names.arg=RF_insula_imp_sorted[1:10,"features"] , ylab="% Increase in Mean Squared Error (Variable Importance)", las=2, ylim=c(0,30000), main="Regression RF Insula")  
@@ -74,7 +72,7 @@ otu_table_scaled_insula_no_features = subset(otu_table_scaled_insula, select = -
 set.seed(123)
 RF_insula_2 <- randomForest( x=otu_table_scaled_insula_no_features[,1:(ncol(otu_table_scaled_insula_no_features)-1)] , 
                            y=otu_table_scaled_insula_no_features$insula , ntree=501, importance=TRUE, proximities=TRUE )  
-RF_insula_2 #%Var -0.04 (no longer large R2)
+RF_insula_2 
 
 saveRDS( file = "RF_insula_2.rda" ,RF_insula_2 )
 
@@ -97,7 +95,7 @@ otu_table_scaled_amygdala$amygdala <- metadata_brain[rownames(otu_table_scaled_a
 set.seed(123)
 RF_amygdala <- randomForest( x=otu_table_scaled_amygdala[,1:(ncol(otu_table_scaled_amygdala)-1)] , 
                              y=otu_table_scaled_amygdala$amygdala , ntree=501, importance=TRUE, proximities=TRUE )  
-RF_amygdala #%Var -3.14
+RF_amygdala
 saveRDS( file = "RF_amygdala.rda" ,RF_amygdala )
 
 
@@ -120,7 +118,7 @@ otu_table_scaled_hippocampus$hippocampus <- metadata_brain[rownames(otu_table_sc
 set.seed(123)
 RF_hippocampus <- randomForest( x=otu_table_scaled_hippocampus[,1:(ncol(otu_table_scaled_hippocampus)-1)] , 
                                 y=otu_table_scaled_hippocampus$hippocampus , ntree=501, importance=TRUE, proximities=TRUE )  
-RF_hippocampus #%V -16.4 
+RF_hippocampus 
 saveRDS( file = "RF_hippocampus.rda" ,RF_hippocampus )
 
 
